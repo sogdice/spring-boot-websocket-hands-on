adb138c99e50bfd55c00e10d344449ed63fa2312
sogdice <sogdice@users.noreply.github.com>
2015-03-10 16:08:52 +0900

    ログの追加, バージョンの変更

--- example-spring-ws-simple/src/main/java/sogdice/example/spring/boot/websocket/simple/Application.java
+++ example-spring-ws-simple/src/main/java/sogdice/example/spring/boot/websocket/simple/Application.java
@@ -1,13 +1,11 @@
 package sogdice.example.spring.boot.websocket.simple;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 @Controller
 public class Application {
 
--- example-spring-ws-simple/src/main/java/sogdice/example/spring/boot/websocket/simple/EchoHandler.java
+++ example-spring-ws-simple/src/main/java/sogdice/example/spring/boot/websocket/simple/EchoHandler.java
@@ -3,6 +3,8 @@ package sogdice.example.spring.boot.websocket.simple;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
@@ -10,25 +12,27 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 public class EchoHandler extends TextWebSocketHandler {
 
+    private static Logger log = LoggerFactory.getLogger(EchoHandler.class);
+
     private Map<String, WebSocketSession> sessionMap = new ConcurrentHashMap<String, WebSocketSession>();
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        log.info("established: {}", session.getId());
         sessionMap.put(session.getId(), session);
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        log.info("closed: {}", session.getId());
         sessionMap.remove(session.getId());
     }
 
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage textMessage) throws Exception {
-        sessionMap.values().forEach(sess -> {
-            try {
-                sess.sendMessage(textMessage);
-            } catch (Exception e) {
-            }
-        });
+        log.info("message(from={}): {}", session.getId(), textMessage.getPayload());
+        for (WebSocketSession sess : sessionMap.values()) {
+            sess.sendMessage(textMessage);
+        }
     }
 }
